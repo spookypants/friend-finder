fefc74306914c0aabaabaef36349277a7c38ef68
Gabe <42557432+spookypants@users.noreply.github.com>
2019-08-15 17:55:36 -0500

    app

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+      name: "Angry Skull",
+      photo: "../data/images/skull3.png",
+      scores: [
+        "5",
+        "3",
+        "2",
+        "2",
+        "5",
+        "5",
+        "1",
+        "4",
+        "3",
+        "3"
+      ]
+    },
+    {
+      name: "Sleepy Skull",
+      photo: "../data/images/skull1.png",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Bemused Skull",
+      photo: "../data/images/skull2.png",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    }  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
